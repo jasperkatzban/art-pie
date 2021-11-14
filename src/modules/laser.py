12a86cc76711c003730f41059addebecd3d4e788
Jasper Katzban <jasperkatzban@gmail.com>
2021-11-14 15:37:13 -0500

    scaffold laser class

--- src/modules/laser.py
+++ src/modules/laser.py
@@ -1,7 +1,17 @@
 from gpiozero import LED
+from utils.constants import GPIO_LASER
 
 class Laser:
     """Handles turning laser on and off"""
 
     def __init__(self):
-        pass
+        """Creates laser object"""
+        self.laser = LED(GPIO_LASER)
+
+    def on(self):
+        """Turns on laser"""
+        self.laser.on()
+
+    def off(self):
+        """Turns off laser"""
+        self.laser.off()
