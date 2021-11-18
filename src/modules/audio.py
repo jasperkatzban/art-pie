b95220493bf95d13deaeb81e7f43a670bfeaedf2
Jasper Katzban <jasperkatzban@gmail.com>
2021-11-18 17:00:47 -0500

    test audio class

--- src/modules/audio.py
+++ src/modules/audio.py
@@ -1,4 +1,5 @@
-import mido, pyo
+from pyo import *
+import numpy as np
 import logging
 
 logger = logging.getLogger(__name__)
@@ -6,5 +7,54 @@ logger = logging.getLogger(__name__)
 class Audio:
     """Handle audio generation"""
 
-    def __init__(self):
+    def __init__(self, env_raspi=True):
         logger.info('Initializing audio module!')
+
+        # Initialize audio server
+        self.s = Server().boot()
+
+        # Get the length of an audio block.
+        self.bs = self.s.getBufferSize()
+
+        # Create a table of length `buffer size` and read it in loop.
+        self.t = DataTable(size=self.bs)
+        self.osc = TableRead(self.t, freq=self.t.getRate(), loop=True, mul=.1).out()
+
+        # Share the table's memory with a numpy array.
+        self.arr = np.asarray(self.t.getBuffer())
+
+        # Make empty array of samples, which we update with time
+        self.curr_samples = []
+
+        # Register the `set_waveform` function to be called at the beginning
+        # of every processing loop.
+        self.s.setCallback(self.set_waveform)
+
+    def view_table(self):
+        """Opens a window to view the current waveform."""
+        self.t.view()
+
+    def open_gui(self):
+        """Initializes gui."""
+        self.s.gui(locals())
+        
+    def set_samples_from_noise(self):
+        """Generates white noise."""
+        self.curr_samples = self.generate_noise(size=self.bs)
+
+    def generate_noise(self, size):
+        """Generates array of noise of specified size"""
+        return np.random.normal(0.0, 1, size=size)
+
+    def set_samples_from_list(self, samples):
+        """Generates sound based on a list of values"""
+        self.curr_samples = samples
+
+    def set_waveform(self):
+        """Sets current sample array to list of values."""
+        self.set_samples_from_noise()
+        self.arr[:] = self.curr_samples
+
+    def get_buffer_size(self):
+        """Returns current buffer size."""
+        return self.bs
--- src/surface_player.py
+++ src/surface_player.py
@@ -3,6 +3,8 @@ import os
 import sys
 import argparse
 import logging
+import numpy as np
+from cv2 import waitKey
 
 # import modules
 from modules.audio import Audio
@@ -10,17 +12,41 @@ from modules.camera import Camera
 from modules.laser import Laser
 from modules.motor import Motor
 
-# TODO: setup logging level as argument
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
 def main(arguments):
 
+    # parse arguments
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
 
+    parser.add_argument("-l", "--local", help="specify local environment (not Raspberry Pi)",
+                    action="store_true")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
     args = parser.parse_args(arguments)
+
+    # specify if not running on raspberry pi, defaults to true
+    ENV_RASPI = False if args.local else True
+
+    # setup logger
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    # intialize camera module, capture and show a single frame
+    camera = Camera(env_raspi=ENV_RASPI)
+    camera.capture_frame()
+    camera.show_frame()
+
+    # initialize audio module, plays noise when you click start
+    audio = Audio(env_raspi=ENV_RASPI)
+    audio.view_table()
+    audio.open_gui()
+
+    # main loop
+    while True:
+        # escape while running using cv2 interrupt
+        if waitKey(1) & 0xFF == ord('q'):
+            break
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
