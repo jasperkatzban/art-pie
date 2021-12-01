1113ea2d546ff2f883c71439a7f6dc03792c8dca
Jasper Katzban <jasperkatzban@gmail.com>
2021-12-01 00:52:58 -0500

    fix hue wrapping in constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -4,11 +4,18 @@ MOTOR_SPEED = 0.5
 
 GPIO_LASER = 14 # pin 8 (GPIO14) on raspberry pi 2B V1.1
 
-POLYFIT_DEG = 10
+POLYFIT_DEG = 5
 
-min_hue, max_hue = 0, 255
+# vals working in a dark room kinda
+# vals if treating red normally
+# min_hue, max_hue = 170, 180
+# min_hue, max_hue = 0, 10
+
+# vals when swapping red and blue
+# in a dark room
+min_hue, max_hue = 130, 180
 min_sat, max_sat = 0, 255
-min_val, max_val = 150, 255
+min_val, max_val = 180, 255
 
 from numpy import array
 THRESHOLD_LOWER = array([min_hue, min_sat, min_val])
