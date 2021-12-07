ea2b4922c13677503edd585d5eb4089d257fd673
Aydin O'Leary <aoleary@olin.edu>
2021-12-07 15:58:33 -0500

    Add support for inputless RasPi environment

--- tests/audio_tests.py
+++ tests/audio_tests.py
@@ -1,5 +1,5 @@
 from pyo import *
-s = Server().boot()
+s = Server(duplex=0, audio='jack' if env_raspi else 'coreaudio').boot()
 mod = Sine(freq=200, mul=50)
 a = Sine(freq=mod + 440, mul=0.1).out()
 s.gui(locals())
