40b13976372efe845b017f33c942faa632fbaff4
Jasper Katzban <jasperkatzban@gmail.com>
2021-12-13 16:08:53 -0500

    move threshold vals to constants.py

--- src/modules/leds.py
+++ src/modules/leds.py
@@ -2,6 +2,8 @@ from .opc import Client
 import logging
 import numpy as np 
 
+from utils.constants import LED_HUE_THRESHOLD_LOWER, LED_HUE_THRESHOLD_UPPER
+
 logger = logging.getLogger(__name__)
 
 class Leds:
@@ -26,10 +28,8 @@ class Leds:
         self.blue = [ (51, 255, 255) ] * self.numLED
         self.green = [ (0, 255, 0) ] * self.numLED
 
-        self.bot_thresh = 0.3
-        self.top_thresh = 0.7    
-
-    ## CALL INSIDE OF MAIN UPDATE FUNCTION
+        self.top_thresh = LED_HUE_THRESHOLD_UPPER    
+        self.bot_thresh = LED_HUE_THRESHOLD_LOWER
 
     def set_hue(self, profile_avg):
         if profile_avg <= self.bot_thresh:
